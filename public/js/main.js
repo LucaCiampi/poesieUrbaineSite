058db03b67792e7409d019b3ff49b3690c9dfe9a
Luca <luca.ciampi@edu.gobelins.fr>
2022-06-22 22:03:04 +0200

    Added other pieces of art JS and padding for responsive

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+})
+
+function nextOeuvreSlide() {
+    console.log("next")
+}
+
+function previousOeuvreSlide() {
+    console.log("prev")
+}
